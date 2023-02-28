407237dccfce697afb681be048dedcbea94a2d03
tuanngoc0298@gmail.com <75868787+tuanngoc0298@users.noreply.github.com>
2023-02-28 16:37:31 +0700

    fix bug

--- data/tabCharts/playlistCharts.js
+++ data/tabCharts/playlistCharts.js
@@ -1,249 +1,143 @@
-
 var listSongCharts = [
     {
-        name: "Chưa Bao Giờ Em Quên",
+        name: "Eyes - Đôi mắt",
+        singers: ["G.Ducky"],
+        path: "./assets/music/listSong4/song1.mp3",
+        image: "./assets/img/music/listSong4/song1.jpg",
         rank: "1",
-        singers: ["Hương Ly"],
-        time: "05:11",
-        path: "./assets/music/listSong1/song1.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song1.jpg"
+        time: "03:00",
     },
     {
-        name: "Yêu Là Cưới",
+        name: "Giàu Vì Bạn Sang Vì Vợ",
+        singers: ["RPT MCK"],
+        path: "./assets/music/listSong4/song2.mp3",
+        image: "./assets/img/music/listSong4/song2.jpg",
         rank: "2",
-        singers: ["Phát Hồ", "X2X", ""],
-        time: "02:59",
-        path: "./assets/music/listSong1/song2.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song2.jpg"
+        time: "03:00",
     },
     {
-        name: "Em Là Con Thuyền Cô Đơn",
+        name: "Walk On Da Street",
+        singers: ["16 Typh", "16 BrT"],
+        path: "./assets/music/listSong4/song3.mp3",
+        image: "./assets/img/music/listSong4/song3.jpg",
         rank: "3",
-        singers: ["Thái Học"],
-        time: "05:04",
-        path: "./assets/music/listSong1/song3.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song3.jpg"
+        time: "03:00",
     },
     {
-        name: "Khuê Mộc Lang",
+        name: "CHƠI",
+        singers: ["HIEUTHUHAI", "MANBO"],
+        path: "./assets/music/listSong4/song4.mp3",
+        image: "./assets/img/music/listSong4/song4.jpg",
         rank: "4",
-        singers: ["Hương Ly", "Jombie"],
-        time: "03:26",
-        path: "./assets/music/listSong1/song4.flac",
-        image: "./assets/img/tabCharts/chartSongRanks/song4.jpg"
+        time: "03:00",
     },
     {
-        name: "Yêu Là Cưới (Remix)",
+        name: "Thủ Đô Cypher",
+        singers: ["RPT Orijinn", "LOW G", "RZMas", "RPT MCK"],
+        path: "./assets/music/listSong4/song5.mp3",
+        image: "./assets/img/music/listSong4/song5.jpg",
         rank: "5",
-        singers: ["Phát Hồ", "DinhLong", "X2X"],
-        time: "03:50",
-        path: "./assets/music/listSong1/song5.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song5.jpg"
+        time: "03:00",
     },
+
     {
-        name: "Thay Lòng",
+        name: "Buông Hàng",
+        singers: ["Young Milo"],
+        path: "./assets/music/listSong4/song6.mp3",
+        image: "./assets/img/music/listSong4/song6.jpg",
         rank: "6",
-        singers: ["DIMZ", "TVk", "NH4T"],
-        time: "04:36",
-        path: "./assets/music/listSong1/song6.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song6.jpg"
+        time: "03:00",
     },
     {
-        name: "Rồi Tới Luôn",
+        name: "Người Chơi Hệ Đẹp",
+        singers: ["16 Typh"],
+        path: "./assets/music/listSong4/song7.mp3",
+        image: "./assets/img/music/listSong4/song7.jpg",
         rank: "7",
-        singers: ["Nal"],
-        time: "04:07",
-        path: "./assets/music/listSong1/song7.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song7.jpg"
+        time: "03:00",
     },
     {
-        name: "Có Một Tình Yêu Gọi Là Chia Tay",
+        name: "Ghé Qua",
+        singers: ["Dick ", "PC", "Tofu"],
+        path: "./assets/music/listSong4/song8.mp3",
+        image: "./assets/img/music/listSong4/song8.jpg",
         rank: "8",
-        singers: ["Tăng Phúc", "Trương Thảo Nhi"],
-        time: "04:18",
-        path: "./assets/music/listSong1/song8.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song8.jpg"
+        time: "03:00",
     },
     {
-        name: "Cưới Luôn Được Không?",
+        name: "Đìu Anh Luôn Giữ Kín Trong Tym",
+        singers: ["RPT MCK", "tlinh", "2pillz "],
+        path: "./assets/music/listSong4/song9.mp3",
+        image: "./assets/img/music/listSong4/song9.jpg",
         rank: "9",
-        singers: ["YuniBoo", "Goctoi Mixer"],
-        time: "04:02",
-        path: "./assets/music/listSong1/song9.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song9.jpg"
+        time: "03:00",
     },
     {
-        name: "Bao Lâu Ta Lại Yêu Một Người",
+        name: "Xích Thêm Chút",
+        singers: ["RPT Groovie", "TLinh", "RPT MCK"],
+        path: "./assets/music/listSong4/song10.mp3",
+        image: "./assets/img/music/listSong4/song10.jpg",
         rank: "10",
-        singers: ["Doãn Hiếu", "B."],
-        time: "03:51",
-        path: "./assets/music/listSong1/song10.flac",
-        image: "./assets/img/tabCharts/chartSongRanks/song10.jpg"
+        time: "03:00",
     },
     {
-        name: "Thương Nhau Tới Bến",
+        name: "Tay To",
+        singers: ["RPT MCK", "RPT PhongKhin"],
+        path: "./assets/music/listSong4/song11.mp3",
+        image: "./assets/img/music/listSong4/song11.jpg",
         rank: "11",
-        singers: ["Nal"],
-        time: "04:55",
-        path: "./assets/music/listSong1/song11.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song11.jpg"
+        time: "03:00",
     },
     {
-        name: "Đông Phai Mờ Dáng Ai",
+        name: "SIMPLE LOVE",
+        singers: ["Obito", "Seachains", "Davis", "Lena"],
+        path: "./assets/music/listSong4/song12.mp3",
+        image: "./assets/img/music/listSong4/song12.jpg",
         rank: "12",
-        singers: ["DatKaa", "QT Beatz"],
-        time: "04:40",
-        path: "./assets/music/listSong1/song12.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song12.jpg"
+        time: "03:00",
     },
     {
-        name: "Nhắn Rằng Anh Nhớ Em",
+        name: "Va Vào Giai Điệu Này",
+        singers: ["RPT MCK"],
+        path: "./assets/music/listSong4/song13.mp3",
+        image: "./assets/img/music/listSong4/song13.jpg",
         rank: "13",
-        singers: ["Đình Dũng", "ACV"],
-        time: "05:12",
-        path: "./assets/music/listSong1/song13.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song13.jpg"
+        time: "03:00",
     },
     {
-        name: "Dịu Dàng Em Đến",
+        name: "Em Gái",
+        singers: ["Blaise", "Low G"],
+        path: "./assets/music/listSong4/song14.mp3",
+        image: "./assets/img/music/listSong4/song14.jpg",
         rank: "14",
-        singers: ["ERIK", "NinjaZ"],
-        time: "03:05",
-        path: "./assets/music/listSong1/song14.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song14.jpg"
+        time: "03:00",
     },
     {
-        name: "Em Hát Ai Nghe",
+        name: "OK",
+        singers: ["Binz"],
+        path: "./assets/music/listSong4/song15.mp3",
+        image: "./assets/img/music/listSong4/song15.jpg",
         rank: "15",
-        singers: ["Orange"],
-        time: "04:39",
-        path: "./assets/music/listSong1/song15.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song15.jpg"
+        time: "03:00",
     },
     {
-        name: "Vậy Là Ta Mất Nhau",
+        name: "Gene",
+        singers: ["Binz"],
+        path: "./assets/music/listSong4/song16.mp3",
+        image: "./assets/img/music/listSong4/song16.jpg",
         rank: "16",
-        singers: ["Khải Đăng"],
-        time: "04:15",
-        path: "./assets/music/listSong1/song16.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song16.jpg"
+        time: "03:00",
     },
     {
-        name: "vâng anh đi đi (liu riu version)",
+        name: "They Said",
+        singers: ["Binz"],
+        path: "./assets/music/listSong4/song17.mp3",
+        image: "./assets/img/music/listSong4/song17.jpg",
         rank: "17",
-        singers: ["Bích Phương"],
-        time: "03:39",
-        path: "./assets/music/listSong1/song17.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song17.jpg"
-    },
-    {
-        name: "Nếu Lúc Trước Em Đừng Tới",
-        rank: "18",
-        singers: ["Phạm Đình Thái Ngân", "Hino"],
-        time: "03:35",
-        path: "./assets/music/listSong1/song18.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song18.jpg"
-    },
-    {
-        name: "Cô Đơn Dành Cho Ai",
-        rank: "19",
-        singers: ["Lee Ken", "Nal"],
-        time: "03:51",
-        path: "./assets/music/listSong1/song19.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song19.jpg"
-    },
-    {
-        name: "MONEY",
-        rank: "20",
-        singers: ["LISA"],
-        time: "02:48",
-        path: "./assets/music/listSong1/song20.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song20.jpg"
-    },
-    {
-        name: "Yêu Mà Chưa Dám Nói",
-        rank: "21",
-        singers: ["Thanh Hưng"],
-        time: "04:25",
-        path: "./assets/music/listSong1/song21.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song21.jpg"
-    },
-    {
-        name: "Độ Tộc 2",
-        rank: "22",
-        singers: ["Độ Mixi", "Pháo", "Phúc Du", "Masew"],
-        time: "03:21",
-        path: "./assets/music/listSong1/song22.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song22.jpg"
-    },
-    {
-        name: "Chiều Đồng Quê",
-        rank: "23",
-        singers: ["H2K"],
-        time: "03:20",
-        path: "./assets/music/listSong1/song23.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song23.jpg"
-    },
-    {
-        name: "Túp Lều Vàng",
-        rank: "24",
-        singers: ["Nguyễn Đình Vũ", "ACV"],
-        time: "03:31",
-        path: "./assets/music/listSong1/song24.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song24.jpg"
-    },
-    {
-        name: "LALISA",
-        rank: "25",
-        singers: ["LISA"],
-        time: "03:21",
-        path: "./assets/music/listSong1/song25.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song25.jpg"
-    },
-    {
-        name: "Sai Cách Yêu",
-        rank: "26",
-        singers: ["Lê Bảo Bình"],
-        time: "05:42",
-        path: "./assets/music/listSong1/song26.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song26.jpg"
-    },
-    {
-        name: "có hẹn với thanh xuân",
-        rank: "27",
-        singers: ["MONSTAR"],
-        time: "03:38",
-        path: "./assets/music/listSong1/song27.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song27.jpg"
-    },
-    {
-        name: "Em Nào Có Tội",
-        rank: "28",
-        singers: ["Thương Võ", "ACV"],
-        time: "04:51",
-        path: "./assets/music/listSong1/song28.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song28.jpg"
-    },
-    {
-        name: "Nhắn Tới Khoảng Trời Em",
-        rank: "29",
-        singers: ["Quân A.P"],
-        time: "03:55",
-        path: "./assets/music/listSong1/song29.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song29.jpg"
-    },
-    {
-        name: "Em Hứa Thế Nào",
-        rank: "30",
-        singers: ["Như Việt", "ACV"],
-        time: "04:25",
-        path: "./assets/music/listSong1/song30.mp3",
-        image: "./assets/img/tabCharts/chartSongRanks/song30.jpg"
+        time: "03:00",
     },
 ];
 
-const SONG_CHARTS_STORAGE_KEY = 'VIK_SONG_CHARTS';
+const SONG_CHARTS_STORAGE_KEY = "VIK_SONG_CHARTS";
 
 localStorage.setItem(SONG_CHARTS_STORAGE_KEY, JSON.stringify(listSongCharts));
-
-
